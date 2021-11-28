c9f07de97ecb725de536cfaca8ae6134c4ee9088
Miguel Barca <miguelbarca.usc@gmail.com>
2021-11-28 19:49:41 +0000

    First test class - Work in progress

---
+++ test/ClinicCalendarTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Test;
+import patientintake.ClinicCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ClinicCalendarTest {
+
+    @Test
+    public void allowEntryOfAnAppointment() {
+        ClinicCalendar calendar = new ClinicCalendar();
+
+    }
+}
